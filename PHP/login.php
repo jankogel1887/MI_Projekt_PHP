d28a5fb58a66820c76a50c29554cd3f4129900d0
Justin Kühner <dergamer846@gmail.com>
2019-11-08 10:55:02 +0100

    edited login.php

--- PHP/login.php
+++ PHP/login.php
@@ -2,46 +2,41 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Anmelden</title>
+    <title>Login</title>
   </head>
   <body>
     <?php
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
-
     if(isset($_POST['submit'])){
       require('MySql.php');
-      $stmt = $mysql-> prepare('SELECT * FROM accounts WHERE USERNAME = :user'); //Username überprüfen
+      $stmt = $mysql->prepare("SELECT * FROM t_login WHERE Benutzername = :user AND Passwort = :password"); //Username überprüfen
       $stmt->bindParam(":user", $_POST["username"]);
+      $stmt->bindParam(":password", $_POST["pw"]);
       $stmt->execute();
       $count = $stmt->rowCount();
-      if($count == 0){
+      if($count == 1){
         //Username ist frei
-
-          if($_POST["pw"] == $_POST["pw2"]){
-            //User anlegen
-            $stmt = $mysql->prepare("INSERT INTO accounts (USERNAME, PASSWORD) VALUES (:user, :pw)");
-            $stmt->bindParam(":user", $_POST["username"]);
-            $hash = password_hash($_POST["pw"], PASSWORD_BCRYPT);
-            $stmt->bindParam(":pw", $hash);
-            $stmt->execute();
-            echo "Dein Account wurde angelegt";
-          } else {
-            echo "Die Passwörter stimmen nicht überein";
-          }
-
+        $row = $stmt->fetch();
+        if(password_verify($_POST["pw"], $row["PASSWORD"])){
+          session_start();
+          $_SESSION["username"] = $row["USERNAME"];
+          header("Location: geheim.php");
+        } else {
+          echo "Der Login ist fehlgeschlagen";
+        }
       } else {
-        echo "Der Username ist bereits vergeben";
+        echo "Der Login ist fehlgeschlagen";
       }
     }
      ?>
-    <h1>Account erstellen</h1>
-    <form action="register.php" method="post">
+    <h1>Anmelden</h1>
+    <form action="index.php" method="post">
       <input type="text" name="username" placeholder="Username" required><br>
       <input type="password" name="pw" placeholder="Passwort" required><br>
-      <button type="submit" name="submit">Erstellen</button>
+      <button type="submit" name="submit">Einloggen</button>
     </form>
     <br>
-    <a href="index.php">Hast du bereits einen Account?</a>
+    <a href="register.php">Noch keinen Account?</a><br>
   </body>
 </html>
