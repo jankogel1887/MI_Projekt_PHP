c6dddf492d7803ab9ef22ad97f38c75fb8d862dd
Justin Kühner <dergamer846@gmail.com>
2019-11-13 12:00:36 +0100

    a

--- PHP/index.php
+++ PHP/index.php
@@ -10,17 +10,17 @@
     ini_set('display_errors', 'on');
     if(isset($_POST['submit'])){
       require('MySql.php');
-      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+      $stmt = $mysql->prepare("SELECT * FROM T_login WHERE Benutzername= :user"); //Username überprüfen
       $stmt->bindParam(":user", $_POST["username"]);
       $stmt->execute();
       $count = $stmt->rowCount();
       if($count == 1){
         //Username ist frei
         $row = $stmt->fetch();
-        if(password_verify($_POST["pw"], $row["PASSWORD"])){
+        if(password_verify($_POST["pw"], $row["Passwort"])){
           session_start();
           $_SESSION["username"] = $row["USERNAME"];
-          header("Location: geheim.php");
+          header("Location: test.html");
         } else {
           echo "Der Login ist fehlgeschlagen";
         }
